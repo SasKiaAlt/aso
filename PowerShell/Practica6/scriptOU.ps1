9a898e2a6b6278563a50875fa630e174864d672b
SasKiaAlt <sasrodfil@alu.edu.gva.es>
2026-01-27 22:27:25 +0100

    Se sube la práctica 6 de PowerShell
    Unidades Organizativas y Usuarios.

---
+++ PowerShell/Practica6/scriptOU.ps1
@@ -0,0 +1,32 @@
+﻿Import-Module ActiveDirectory
+
+$OUraiz = "Empresa"
+$DNraiz = "OU=Empresa,DC=empresa,DC=local"
+
+$pass = ConvertTo-SecureString "aso2025." -AsPlainText -Force
+
+$deptos = Import-Csv -Path "C:\Users\Administrator\Desktop\Practica6\departamentos.csv"
+
+$empleados = Import-Csv -Path "C:\Users\Administrator\Desktop\Practica6\empleados.csv"
+
+foreach ($dept in $deptos) {
+    $ouNom = $dept.departamento
+    $ouDescrip = $dept.descripcion
+
+    New-ADOrganizationalUnit -Name $ouNom -Path $DNraiz -Description $ouDescrip -ProtectedFromAccidentalDeletion $true
+
+    Write-Host "Sub-OU '$ouNom' creada bajo '$OUraiz'."
+}
+
+foreach ($emplea in $empleados) {
+    $depar = $emplea.departamento
+    $nombre = $emplea.nombre
+    $apellido = $emplea.apellido
+    
+
+    $login = ($nombre + "." + $apellido).ToLower()
+    $ouDN = "OU=$depar,$DNraiz"
+
+     New-ADUser -Name "$nombre $apellido" -GivenName $nombre -Surname $apellido -SamAccountName $login -UserPrincipalName "$login@empresa.local" -AccountPassword $pass -ChangePasswordAtLogon $true -Enabled $true -Path $ouDN
+     Write-Host "Usuario '$login' creado en '$depar'."
+}
